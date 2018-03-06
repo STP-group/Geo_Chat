13477d26993790663326a01e176b51a7224b48f7
Psilck <swiftteamproject@gmail.com>
2018-03-06 20:56:49 +0300

    ver. 0.1
    Добавлено: LoginViewController и RegisteryViewController

---
+++ Geo_Chat/StartMain/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Geo_Chat
+//
+//  Created by Артем Валерьевич on 06.03.2018.
+//  Copyright © 2018 Артем Валерьевич. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+    @IBAction func loginAuthentication(_ sender: UIButton) {
+    }
+    
+
+}
---
+++ Geo_Chat/StartMain/RegisteryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisteryViewController.swift
+//  Geo_Chat
+//
+//  Created by Артем Валерьевич on 06.03.2018.
+//  Copyright © 2018 Артем Валерьевич. All rights reserved.
+//
+
+import UIKit
+
+class RegisteryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
