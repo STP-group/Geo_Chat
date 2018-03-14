225f67d91ab7b70fe16b9b06487d1a6ef70c5949
Psilck <swiftteamproject@gmail.com>
2018-03-14 17:47:10 +0300

    New logic project
    Fix chat VC

--- Geo_Chat/ListChat/ChatViewController.swift
+++ Geo_Chat/ListChat/ChatViewController.swift
@@ -9,30 +9,33 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITextViewDelegate {
     
     struct Messages {
         let message: String
         let email: String
         let date: String
+        let indexMessage: String
         let ref: DatabaseReference?
-        //let refObser: DatabaseReference?
         
         
-        init(message: String, email: String, date: String) {
+        init(message: String, email: String, date: String, indexMessage: String) {
             self.message = message
             self.email = email
             self.date = date
+            self.indexMessage = indexMessage
             self.ref = nil
         }
         init(snapshot: DataSnapshot) {
             let snapshotValue = snapshot.value as! [String: AnyObject]
             email = snapshotValue["email"] as! String
             message = snapshotValue["message"] as! String
+            indexMessage = snapshotValue["indexMessage"] as! String
             date = snapshotValue["date"] as! String
             ref = snapshot.ref
         }
     }
+    
     
     var indexes = [String]()
     var isScrollChatNeeded = false
@@ -43,18 +46,18 @@ class ChatViewController: UIViewController {
     var ref: DatabaseReference!
     var userId = Array<Contact>()
     var message = Array<Messages>()
+    //var indexMessage = "0"
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageTextField: UITextField!
-
-    
+    @IBOutlet weak var messageTextField: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.estimatedRowHeight = 44
+        //self.scrollChatToLastRowIfNeeded()
+        tableView.estimatedRowHeight = 70
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.separatorColor = .clear
         print(nameVC)
+        print(idUser)
         
         guard let currentUser = Auth.auth().currentUser else { return }
         // user
@@ -80,21 +83,21 @@ class ChatViewController: UIViewController {
         let hour = calendar.component(.hour, from: date)
         let minutes = calendar.component(.minute, from: date)
         let seconds = calendar.component(.second, from: date)
-        // let miliSeconds = calendar.component(.nanosecond, from: date)
-        let timeMessages = "\(hour):\(minutes):\(seconds)"
+         let miliSeconds = calendar.component(.nanosecond, from: date)
+        let timeMessages = "\(hour):\(minutes):\(seconds):\(miliSeconds)"
         return timeMessages
     }
     
     @IBAction func sendMessage(_ sender: UIButton) {
         
         guard messageTextField.text != "" else { return }
-        let text = Messages(message: messageTextField.text!, email: idUser, date: dateMessage())
-        //let text = Messages(message: messageTextField.text!, email: idUser, )
+        let text = Messages(message: messageTextField.text!, email: idUser, date: dateMessage(), indexMessage: String(message.count))
         print(date())
-        let refMessage = self.ref.child(date())
-        refMessage.setValue(["message": text.message, "email": text.email, "date": text.date])
+        let refMessage = self.ref.child(String(message.count))
+        refMessage.setValue(["message": text.message, "email": idUser, "date": text.date, "indexMessage": String(message.count)])
+        //refMessage.setValue(["message": text.message, "email": idUser, "date": text.date, "indexMessage": message.count])
         messageTextField.text = ""
-        self.scrollChatToLastRowIfNeeded()
+        
         
     }
     
@@ -115,16 +118,23 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
             }
             
             self?.message = _list
+            
             self?.tableView.reloadData()
+
             self?.scrollChatToLastRowIfNeeded()
+            
+
         }
     }
     
     func scrollChatToLastRowIfNeeded() {
-        if isScrollChatNeeded || isScrollChatForced {
+        if message.count <= 0  {
+            return
+        } else {
             let indexPath = IndexPath(row: message.count - 1, section: 0)
             tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
-            isScrollChatNeeded = false
+            
+            self.message.sort {$0.self.indexMessage < $1.self.indexMessage }
         }
     }
     
@@ -133,7 +143,10 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
         return message.count
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //scrollChatToLastRowIfNeeded()
         if message[indexPath.row].email == idUser {
             let cell = tableView.dequeueReusableCell(withIdentifier: "inCell", for: indexPath) as! FrendsMessageCell
             
